367a55782bae042bd82616d99b7ddcb4bd79f2b1
kempie12 <onaboluoluwakememi@gmail.com>
2022-10-13 12:03:54 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printingf
+ * Return: Always 0 (Okay)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
