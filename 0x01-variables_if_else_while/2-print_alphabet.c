efbbb8bc0fce2f33ca40e5e6290f7ed6286c7510
kempie12 <onaboluoluwakememi@gmail.com>
2022-10-14 14:19:20 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabets
+ *
+ * Return: Always 0 (Okay)
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
